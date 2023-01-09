b622916c205a4b290cc945a3b419714dcc378504
brekkanegg <brekkanegg@gmail.com>
2023-01-09 23:51:50 +0900

    some more

--- contracts/BuffNFT.sol
+++ contracts/BuffNFT.sol
@@ -2,15 +2,18 @@
 pragma solidity ^0.8.17;
 
 import "hardhat/console.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract BuffNFT is ERC721URIStorage, Ownable {
+contract BuffNFT is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIdCounter;
 
-    mapping(address => bool) public isAllowlistAddress;
+    // mapping(address => bool) public isAllowlistAddress;
 
     constructor() ERC721("BuFfToken", "BFT") {}
 
@@ -23,34 +26,63 @@ contract BuffNFT is ERC721URIStorage, Ownable {
     //     }
     // }
 
-    function airdropNFTs(address[] calldata wAddresses) public onlyOwner {
+    // function airdropNFTs(address[] calldata wAddresses) public onlyOwner {
+    //     for (uint i = 0; i < wAddresses.length; i++) {
+    //         _mintSingleNFT(wAddresses[i]);
+    //     }
+    // }
+
+    // function _mintSingleNFT(address wAddress) private {
+    //     uint tokenId = _tokenIdCounter.current();
+    //     _tokenIdCounter.increment();
+    //     _safeMint(wAddress, tokenId);
+    // }
+
+    function airdropNFTs(address[] calldata wAddresses, string[] calldata tokenUris) public onlyOwner {
+        require(wAddresses.length == tokenUris.length);
         for (uint i = 0; i < wAddresses.length; i++) {
-            _mintSingleNFT(wAddresses[i]);
+            _mintSingleNFT(wAddresses[i], tokenUris[i]);
         }
     }
 
-    function _mintSingleNFT(address wAddress) private {
+    function _mintSingleNFT(address wAddress, string memory tokenUri) private {
         uint tokenId = _tokenIdCounter.current();
         _tokenIdCounter.increment();
         _safeMint(wAddress, tokenId);
+        _setTokenURI(tokenId, tokenUri);
     }
 
-    // // Presale mint
-    // function preSale() public {
-    //     require(isAllowlistAddress[msg.sender], "Address is not allowlisted");
+    // The following functions are overrides required by Solidity.
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 tokenId
+    ) internal override(ERC721, ERC721Enumerable) {
+        super._beforeTokenTransfer(from, to, tokenId);
+    }
 
-    //     for (uint i = 0; i < 2; i++) {
-    //         _mintSingleNFT();
-    //     }
+    function _burn(uint256 tokenId)
+        internal
+        override(ERC721, ERC721URIStorage)
+    {
+        super._burn(tokenId);
+    }
 
-    //     console.log("2 NFTs minted using allowlist.");
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
 
-    //     isAllowlistAddress[msg.sender] = false;
-    // }
-
-    // function _mintSingleNFT() private {
-    //     uint newTokenID = _tokenIds.current();
-    //     _safeMint(msg.sender, newTokenID);
-    //     _tokenIds.increment();
-    // }
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Enumerable)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
 }
